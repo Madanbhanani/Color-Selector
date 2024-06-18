99f88ef5eba3ef262d9aee3436fecabfc2c5ae69
Madanbhanani <bhananimadan@gmail.com>
2024-06-17 23:11:53 -0700

    color selector

---
+++ index.js
@@ -0,0 +1,107 @@
+
+function red() {
+    document.getElementById("cnt").style.background = "red"
+    document.getElementById("colorname").innerHTML = "red"
+}
+function green() {
+    document.getElementById("cnt").style.background = "green"
+    document.getElementById("colorname").innerHTML = "green"
+}
+function blue() {
+    document.getElementById("cnt").style.background = "blue"
+    document.getElementById("colorname").innerHTML = "blue"
+}
+function black() {
+    document.getElementById("cnt").style.background = "black"
+    document.getElementById("colorname").innerHTML = "black"
+    document.getElementById("colorname").style.color = "white"
+}
+function yellow() {
+    document.getElementById("cnt").style.background = "yellow"
+    document.getElementById("colorname").innerHTML = "yellow"
+}
+function orange() {
+    document.getElementById("cnt").style.background = "orange"
+    document.getElementById("colorname").innerHTML = "orange"
+}
+function brown() {
+    document.getElementById("cnt").style.background = "brown"
+    document.getElementById("colorname").innerHTML = "brown"
+}
+function aqua() {
+    document.getElementById("cnt").style.background = "aqua"
+    document.getElementById("colorname").innerHTML = "aqua"
+}
+function cyan() {
+    document.getElementById("cnt").style.background = "cyan"
+    document.getElementById("colorname").innerHTML = "cyan"
+}
+function grey() {
+    document.getElementById("cnt").style.background = "grey"
+    document.getElementById("colorname").innerHTML = "grey"
+}
+function Pink() {
+    document.getElementById("cnt").style.background = "Pink"
+    document.getElementById("colorname").innerHTML = "pink"
+}
+function teal() {
+    document.getElementById("cnt").style.background = "teal"
+    document.getElementById("colorname").innerHTML = "teal"
+}
+function Maroon() {
+    document.getElementById("cnt").style.background = "Maroon"
+    document.getElementById("colorname").innerHTML = "maroon"
+}
+function Salmon() {
+    document.getElementById("cnt").style.background = "Salmon"
+    document.getElementById("colorname").innerHTML = "salmon"
+}
+function indigo() {
+    document.getElementById("cnt").style.background = "indigo"
+    document.getElementById("colorname").innerHTML = "indigo"
+}
+function Purple() {
+    document.getElementById("cnt").style.background = "Purple"
+    document.getElementById("colorname").innerHTML = "purple"
+}
+function olive() {
+    document.getElementById("cnt").style.background = "olive"
+    document.getElementById("colorname").innerHTML = "olive"
+}
+function coral() {
+    document.getElementById("cnt").style.background = "coral"
+    document.getElementById("colorname").innerHTML = "coral"
+}
+function olive() {
+    document.getElementById("cnt").style.background = "olive"
+    document.getElementById("colorname").innerHTML = "olive"
+}
+function deeppink() {
+    document.getElementById("cnt").style.background = "deeppink"
+    document.getElementById("colorname").innerHTML = "deeppink"
+}
+function blueviolet() {
+    document.getElementById("cnt").style.background = "blueviolet"
+    document.getElementById("colorname").innerHTML = "blueviolet"
+}
+function tan() {
+    document.getElementById("cnt").style.background = "tan"
+    document.getElementById("colorname").innerHTML = "tan"
+}
+function cadetblue() {
+    document.getElementById("cnt").style.background = "cadetblue"
+    document.getElementById("colorname").innerHTML = "cadetblue"
+}
+function chartreuse() {
+    document.getElementById("cnt").style.background = "chartreuse"
+    document.getElementById("colorname").innerHTML = "chartreuse"
+}
+function fuchsia() {
+    document.getElementById("cnt").style.background = "fuchsia"
+    document.getElementById("colorname").innerHTML = "fuchsia"
+}
+
+function sienna() {
+    document.getElementById("cnt").style.background = "sienna"
+    document.getElementById("colorname").innerHTML = "sienna"
+}
